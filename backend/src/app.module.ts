4a83586b7fe605e62d0df7bb9e6f2bb2b4dfe9ec
Antonio L <antonio.loncar@inftec.ch>
2022-02-07 17:41:48 +0100

    add request logger middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
