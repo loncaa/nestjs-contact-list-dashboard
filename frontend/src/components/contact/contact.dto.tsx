509b929e77c68d4b3f952cecd6b8597294878df0
Antonio L <antonio.loncar@inftec.ch>
2022-02-08 12:03:06 +0100

    move types to one file

--- frontend/src/components/contact/contact.dto.tsx
+++
@@ -1,6 +0,0 @@
-export type CreateContactDTO = {
-  name: string;
-  email: string;
-  address: string;
-  phone: string;
-};
--- frontend/src/components/contact/contact.type.tsx
+++ frontend/src/components/contact/contact.type.tsx
@@ -1,12 +1,19 @@
-export interface Contact {
+export type ContactDTO = {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+};
+
+export type Contact = {
   id: string;
   readonly name: string;
   readonly email: string;
   readonly address: string;
   readonly phone: string;
-}
+};
 
-export interface ContactsResponseObject {
+export type ContactsRes = {
   contacts: Contact[];
   totalPages: number;
-}
+};
