39574e879d573351947a2f2bf6af331b93716fa5
Antonio L <antonio.loncar@inftec.ch>
2022-02-09 11:05:39 +0100

    allow empty email an address field

--- backend/src/contact/validation/contact.schema.ts
+++ backend/src/contact/validation/contact.schema.ts
@@ -9,7 +9,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().max(20).optional(),
-  email: Joi.string().max(20).optional(),
+  email: Joi.string().max(20).optional().allow(null, ""),
   phone: Joi.string().max(20).optional(),
-  address: Joi.string().max(20).optional(),
+  address: Joi.string().max(20).optional().allow(null, ""),
 });
