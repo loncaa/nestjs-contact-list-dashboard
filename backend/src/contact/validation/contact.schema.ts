0e21457e95ef441d47342fc18c03ab1142d6f460
Antonio L <antonio.loncar@inftec.ch>
2022-02-09 12:43:50 +0100

    allow null and empty string for email and address

--- backend/src/contact/validation/contact.schema.ts
+++ backend/src/contact/validation/contact.schema.ts
@@ -2,14 +2,14 @@ import * as Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().max(20).required(),
-  email: Joi.string().max(20).optional(),
+  email: Joi.string().max(20).optional().allow(null, ''),
   phone: Joi.string().max(20).required(),
-  address: Joi.string().max(20).optional(),
+  address: Joi.string().max(20).optional().allow(null, ''),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().max(20).optional(),
-  email: Joi.string().max(20).optional().allow(null, ""),
+  email: Joi.string().max(20).optional().allow(null, ''),
   phone: Joi.string().max(20).optional(),
-  address: Joi.string().max(20).optional().allow(null, ""),
+  address: Joi.string().max(20).optional().allow(null, ''),
 });
