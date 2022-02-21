20b9934d974c21bcd2b85fe5ed95af1a6c7cfb89
Antonio <--local>
2022-02-21 20:55:19 +0100

    formatted

--- frontend/src/containers/editProfile/index.js
+++ frontend/src/containers/editProfile/index.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import EditProfile from '../../components/editProfile';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as contactActions from '../../redux/contact/contactActions';
 
 import * as ContactService from '../../service/contactService'
-import {handleValidation} from '../../validators/contactFormValidation';
-import {createNumber} from '../../service/contactService';
+import { handleValidation } from '../../validators/contactFormValidation';
 import EditProfileNotification
   from '../../components/editProfile/EditProfileNotification';
 
@@ -18,27 +17,28 @@ class Index extends Component {
   }
 
   componentDidMount() {
-    if(this.props.contact && this.props.contact.selectedContact) {
+    if (this.props.contact && this.props.contact.selectedContact) {
 
       //deep copy
       const stringify = JSON.stringify(this.props.contact.selectedContact)
       const newObject = JSON.parse(stringify)
       const dirtyContact = newObject;
 
-      this.setState({dirtyContact: dirtyContact})
-    }else if(this.props.contact.selectedContact === null){
-      const newContact = ContactService.createContactProfile('Full name', '', false, 'Email', [createNumber('Cell', 'Number')]);
-      this.setState({dirtyContact: newContact})
+      this.setState({ dirtyContact: dirtyContact })
+    } else if (this.props.contact.selectedContact === null) {
+      const newContact = ContactService.createContactProfile('Full name', null, false, 'Email', [ContactService.createNumber('Cell', 'Number')]);
+      this.setState({ dirtyContact: newContact })
     }
   }
 
   onContactDataSave = () => {
-    if(this.state.dirtyContact){
+    if (this.state.dirtyContact) {
       const validationResponse = handleValidation(this.state.dirtyContact)
-      if(validationResponse.valid){
-        this.props.actions.contact.saveContact(this.state.dirtyContact);
-        this.setState({notificationOpen: true});
-      }else{
+      if (validationResponse.valid) {
+        const payload = ContactService.createContactProfilePayload(this.state.dirtyContact)
+        this.props.actions.contact.saveContact(payload);
+        this.setState({ notificationOpen: true });
+      } else {
         alert(`Field ${validationResponse.field} not valid: ${validationResponse.message}`);
       }
     }
@@ -47,41 +47,41 @@ class Index extends Component {
   onChange = (e) => {
     const { name, value } = e.target
 
-    if(this.state.dirtyContact){
-      const contact = {...this.state.dirtyContact}
+    if (this.state.dirtyContact) {
+      const contact = { ...this.state.dirtyContact }
       contact[name] = value;
-      this.setState({dirtyContact: contact})
+      this.setState({ dirtyContact: contact })
     }
   }
 
   onNumbersChange = (e, i) => {
     const { name, value } = e.target
 
-    if(this.state.dirtyContact){
-      const contact = {...this.state.dirtyContact}
+    if (this.state.dirtyContact) {
+      const contact = { ...this.state.dirtyContact }
       contact.numbers[i][name] = value;
-      this.setState({dirtyContact: contact})
+      this.setState({ dirtyContact: contact })
     }
   }
 
   onNumberRemove = (i) => {
-    if(this.state.dirtyContact){
-      const contact = {...this.state.dirtyContact}
-      contact.numbers.splice(i,1);
-      this.setState({dirtyContact: contact})
+    if (this.state.dirtyContact) {
+      const contact = { ...this.state.dirtyContact }
+      contact.numbers.splice(i, 1);
+      this.setState({ dirtyContact: contact })
     }
   }
 
   onNewNumberAdded = () => {
-    if(this.state.dirtyContact){
-      const contact = {...this.state.dirtyContact}
-      contact.numbers.push(ContactService.createNumber('',''));
-      this.setState({dirtyContact: contact})
+    if (this.state.dirtyContact) {
+      const contact = { ...this.state.dirtyContact }
+      contact.numbers.push(ContactService.createNumber('', ''));
+      this.setState({ dirtyContact: contact })
     }
   }
 
-  handleNotificationClose = () =>{
-    this.setState({notificationOpen: false});
+  handleNotificationClose = () => {
+    this.setState({ notificationOpen: false });
   }
 
   render() {
@@ -102,7 +102,7 @@ class Index extends Component {
           dirty={this.state.dirtyContact}
         />
       </React.Fragment>
-  );
+    );
   }
 }
 
