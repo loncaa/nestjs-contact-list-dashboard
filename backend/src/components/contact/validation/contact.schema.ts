c86a3e309d726023955a0486eb84033c6902e071
Antonio <--local>
2022-02-24 23:20:20 +0100

    extend joi with sanitize url

--- backend/src/components/contact/validation/contact.schema.ts
+++ backend/src/components/contact/validation/contact.schema.ts
@@ -1,5 +1,27 @@
 import * as Joi from 'joi';
 
+interface ExtendedStringSchema extends Joi.StringSchema {
+  urlSanitize(): this;
+}
+
+interface UrlSanitizeJoi extends Joi.Root {
+  string(): ExtendedStringSchema;
+}
+
+const CustomJoi: UrlSanitizeJoi = Joi.extend((joi) => {
+  return {
+    type: 'string',
+    base: joi.string(),
+    rules: {
+      urlSanitize: {
+        validate(value) {
+          return encodeURI(value.trim());
+        },
+      },
+    },
+  };
+});
+
 export const contactNumberSchema = Joi.object({
   type: Joi.string().required(),
   number: Joi.string().required()
@@ -7,16 +29,16 @@ export const contactNumberSchema = Joi.object({
 
 export const createContactSchema = Joi.object({
   name: Joi.string().max(20).required(),
-  email: Joi.string().max(20).optional().allow(null, ''),
-  profilePicture: Joi.string().uri().required(),
+  email: Joi.string().email().max(20).optional().allow(null, ''),
+  profilePicture: CustomJoi.string().urlSanitize().uri().required(),
   isFavorite: Joi.boolean(),
   numbers: Joi.array().items(contactNumberSchema).allow(null),
-  address: Joi.string().max(20).optional().allow(null, ''),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().max(20).optional(),
-  email: Joi.string().max(20).optional().allow(null, ''),
+  email: Joi.string().email().max(20).optional().allow(null, ''),
   numbers: Joi.array().items(contactNumberSchema).allow(null),
-  address: Joi.string().max(20).optional().allow(null, ''),
+  profilePicture: CustomJoi.string().urlSanitize().uri(),
+  isFavorite: Joi.boolean(),
 });
--- backend/src/pipes/joiValidation.pipe.ts
+++ backend/src/pipes/joiValidation.pipe.ts
@@ -3,12 +3,12 @@ import { ObjectSchema } from 'joi';
 
 @Injectable()
 export class JoiValidationPipe implements PipeTransform {
-  constructor(private schema: ObjectSchema) {}
+  constructor(private schema: ObjectSchema) { }
 
   transform(value: any, metadata: ArgumentMetadata) {
-    if(metadata.type === 'body'){
-      const { error } = this.schema.validate(value);
-      if (error) {
+    if (metadata.type === 'body') {
+      const response = this.schema.validate(value);
+      if (response.error) {
         throw new BadRequestException('Validation failed');
       }
       return value;
